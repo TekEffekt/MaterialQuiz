0eedc4c8d129de232a4d5b9a4b00a9c31b1ee731
Kyle K Zawacki <z95sk8@outlook.com>
2015-10-14 12:27:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kylez/quizapp/QuizActivity.java
@@ -0,0 +1,43 @@
+package com.example.kylez.quizapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+
+import io.codetail.animation.SupportAnimator;
+import io.codetail.animation.ViewAnimationUtils;
+
+public class QuizActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+
+    public void performSplash(View v)
+    {
+        final View myView = v;
+        myView.setAlpha(1);
+
+        int cx = (myView.getLeft() + myView.getRight()) / 2;
+        int cy = (myView.getTop() + myView.getBottom()) / 2;
+
+        int radius = Math.max(myView.getWidth(), myView.getHeight());
+
+        SupportAnimator animator =
+                ViewAnimationUtils.createCircularReveal(myView, cx, cy, 0, radius);
+
+        animator.setInterpolator(new AccelerateDecelerateInterpolator());
+        animator.setDuration(500);
+        animator.start();
+//
+        Log.d("Debug","What");
+    }
+
+}
